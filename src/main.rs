1ed8416a7132afad04980bd908a669058e25a812
Sarah Connor <14093193+terminatingcode@users.noreply.github.com>
2026-02-04 17:01:49 +0000

    add hello world with async-graphql

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use async_graphql::*;
+
+struct Query;
+
+#[Object]
+impl Query {
+    async fn hello(&self, name: String) -> String {
+        format!("Hello {}", name)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    trpl::run(async {
+        let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
+        let res = schema.execute("{ hello(name: \"Sarah\") }").await;
+
+        let json = serde_json::to_string(&res);
+        println!("{}", json.unwrap());
+    });
+
+    println!("Goodbye, world!");
 }
